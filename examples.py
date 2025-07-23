0fd18f21016a49d4be3b46eca90c14af610feffe
unknown <melkywafula29@gmail.com>
2025-07-23 21:17:51 +0300

    more changes

--- examples.py
+++ examples.py
@@ -3,6 +3,7 @@ age = 25          # int (integer)
 height = 5.75      # float (decimal)
 name = "Charlie"   # str (string)
 Grade = "A"          # str (string)
+Stream = "Computer Science"  # str (string)
 # Boolean variable to indicate if the person is a student
 is_student = True  # bool (boolean)
 
@@ -11,4 +12,5 @@ print("Name:", name)          # Outputs: Name: Charlie
 print("Age:", age)            # Outputs: Age: 25
 print("Height:", height)      # Outputs: Height: 5.75
 print("Grade:", Grade)        # Outputs: Grade: A
+print("Stream:", Stream)      # Outputs: Stream: Computer Science
 print("Is a student?", is_student)  # Outputs: Is a student? True
